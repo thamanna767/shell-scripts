e0358b6ac067b227e54924c0be50ca80288f7e7f
thamanna767 <thamannafathima37@gmail.com>
2026-02-08 11:38:37 +0530

    shell scripts added

--- display_info.sh
+++ display_info.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-
-echo "Name: Thamanna Fathima A M"
+echo "Name:Thamanna Fathima A M"
 echo "Roll Number: 60"
-echo "Class: EV4"
-echo "Current Date: $(date '+%A, %B %d, %Y')"
-echo "Current Time: $(date '+%H:%M:%S %Z')"
+echo "Class:60"
+echo "     DATE & TIME INFORMATION"
+echo "Date & Time : $(date)"
 
